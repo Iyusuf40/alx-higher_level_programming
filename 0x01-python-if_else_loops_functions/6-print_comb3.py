016f8c7e3d214c9d1744c920f4ffa4791df07d04
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-05-31 21:09:28 +0000

    print all possible nonrepeating comb of 2 numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for first_n in range(0, 10):
+    for sec_n in range (0, 10):
+        if first_n != sec_n and first_n < sec_n:
+            if first_n != 8 and sec_n != 9:
+                print("{}{}, ".format(first_n, sec_n), end="")
+            elif first_n == 8 and sec_n == 9:
+                 print("{}{}".format(first_n, sec_n))
