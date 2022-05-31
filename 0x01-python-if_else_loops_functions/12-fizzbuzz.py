bcb01d4f2257ff1ab9ae8f1751cab6225263071f
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-05-31 22:31:38 +0000

    edit fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for _ in range(1, 101):
-        if _ != 100:
-            if _ % 3 == 0 and _ % 5 == 0:
-                print("{} ".format("FizzBuzz"), end="")
-            elif _ % 3 == 0:
-                print("{} ".format("Fizz"), end="")
-            elif _ % 5 == 0:
-                print("{} ".format("Buzz"), end="")
-            else:
-                print("{} ".format(_), end="")
-    print("{} ".format("Buzz"))
+        if _ % 3 == 0 and _ % 5 == 0:
+            print("{} ".format("FizzBuzz"), end="")
+        elif _ % 3 == 0:
+            print("{} ".format("Fizz"), end="")
+        elif _ % 5 == 0:
+            print("{} ".format("Buzz"), end="")
+        else:
+            print("{} ".format(_), end="")
