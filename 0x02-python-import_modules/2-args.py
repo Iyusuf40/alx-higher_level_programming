788c5b804bb7e77dcf6bfa72a976e63928fcebd0
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-02 11:56:09 +0000

    sys.argv var

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-i = 0
-list_ = sys.argv
-for _ in list_:
-    i += 1
-if i == 1:
-    print("0 arguments.")
-else:
-    index = 1
-    print("{i} arguments:".format(i = i - 1))
-    for _ in range (1, i):
-        print("{}: {}".format(index, list_[index]))
-        index += 1
+if __name__ == "__main__":
+    import sys
+    i = 0
+    list_ = sys.argv
+    for _ in list_:
+        i += 1
+    if i == 1:
+        print("0 arguments.")
+    else:
+        index = 1
+        print("{i} arguments:".format(i = i - 1))
+        for _ in range (1, i):
+            print("{}: {}".format(index, list_[index]))
+            index += 1
