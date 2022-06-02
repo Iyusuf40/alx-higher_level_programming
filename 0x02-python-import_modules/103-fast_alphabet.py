c54e425db086699b9dcb4c6d90775b621510a0c6
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-02 13:55:07 +0000

    print from module

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from imp_103 import letters_upper
+letters_upper()
---
+++ 0x02-python-import_modules/imp_103.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def letters_upper():
+    for _ in range (65, 91):
+        print(chr(_), end="")
+    print()
