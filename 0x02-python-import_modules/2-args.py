91fd20a09842d681c5c057827977f4516703ccce
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-02 11:57:56 +0000

    fix pycodesty;e

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         index = 1
-        print("{i} arguments:".format(i = i - 1))
-        for _ in range (1, i):
+        print("{i} arguments:".format(i=i - 1))
+        for _ in range(1, i):
             print("{}: {}".format(index, list_[index]))
             index += 1
