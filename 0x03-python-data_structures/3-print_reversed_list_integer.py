0d981eb639eb346dc180ed8a35f79a7062a1122e
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-04 11:57:29 +0000

    handle empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        return
     my_list = reversed(my_list)
-
     for _ in my_list:
         print("{:d}".format(_))
 
 
 if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
+    my_list = []
     print_reversed_list_integer(my_list)
