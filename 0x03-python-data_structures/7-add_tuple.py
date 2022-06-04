ee61ef64ea89fbe7f01b93d4f23fd82138f8210a
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-04 14:00:28 +0000

    add function that adds from a tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) >= 2:
-        a, b = tuple_a
+        a, b = tuple_a[0], tuple_a[1]
     elif len(tuple_a) == 1:
         a, b = tuple_a[0], 0
     else:
         a, b = 0, 0
 
     if len(tuple_b) >= 2:
-        x, y = tuple_b
+        x, y = tuple_b[0], tuple_b[1]
     elif len(tuple_b) == 1:
         x, y = tuple_b[0], 0
     else:
