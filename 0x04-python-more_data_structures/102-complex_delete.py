69318e16a9966db63ca2b4644ce75750e0d46572
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-08 22:27:12 +0000

    handle None

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     list_ = []
-    if not a_dictionary len(a_dictionary) == 0:
-        return
+    if not a_dictionary or len(a_dictionary) == 0:
+        return a_dictionary
     if not value:
         return a_dictionary
     for x, y in a_dictionary.items():
