8a95691b7acee1faca97249fb66921e1faecea1c
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-08 22:19:38 +0000

    add a function that deletes multiple items in dict

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    list_ = []
+    for x, y in a_dictionary.items():
+        if value == y:
+            list_.append(y)
+    if len(list_) == 0:
+        return a_dictionary
+    for item in list_:
+        del a_dictionary[item]
+    return a_dictionary
