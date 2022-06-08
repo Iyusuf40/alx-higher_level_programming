34c12a53569aa69b5cc873a2d31d7953b3b9c19e
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-08 17:13:35 +0000

    add a function that returns the count of number of keys in adictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    i = 0
+    for _ in a_dictionary:
+        i += 1
+    return i
+
+
+if __name__ == "__main__":
+    a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
+    nb_keys = number_keys(a_dictionary)
+    print("Number of keys: {:d}".format(nb_keys))
