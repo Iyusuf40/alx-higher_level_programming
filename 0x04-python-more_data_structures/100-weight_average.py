e3739bab13f7de939747739f913efbaa667d1800
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-08 21:48:08 +0000

    fix empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
     return (sum(list(map(lambda x: x[0] * x[1], my_list))) /
             sum(list(map(lambda x: x[1], my_list))))
 
