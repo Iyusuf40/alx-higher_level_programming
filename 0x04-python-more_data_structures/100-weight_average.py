e37e0359448c1cb154d6c76fe86f9b23bc51f439
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-08 21:46:07 +0000

    first advanced task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    return (sum(list(map(lambda x: x[0] * x[1], my_list))) /
+            sum(list(map(lambda x: x[1], my_list))))
+
+
+if __name__ == "__main__":
+    my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+    # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+    result = weight_average(my_list)
+    print("Average: {:0.2f}".format(result))
