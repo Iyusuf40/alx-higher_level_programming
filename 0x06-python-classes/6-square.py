62c6a2c021863f940116eb32410c2c4afe10c844
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-22 06:31:16 +0000

    handle negative values

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,7 +19,7 @@ class Square:
         if not isinstance(value, tuple) or len(value) is not 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         x, y = value
-        if not isinstance(x, int) or not isinstance(y, int):
+        if not isinstance(x, int) or not isinstance(y, int) or y < 0 or x < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
