949e12d3e896043c87f8e99bbf04b7e56e25d219
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-22 20:26:19 +0000

    test

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+#!usr/bin/python3
+import math
+"""magic class"""
+
+
+class MagicClass:
+    """testing testing"""
+
+    def __init__(self, radius=0):
+        # self.radius = radius
+
+        if type(radius) is not type(4) and type(radius) is not type(4.0):
+            raise TypeError("radius must be a number")
+        self.radius = radius
+
+    def area(self):
+        """area of a circle"""
+
+        return math.pi ** 2 * self.radius
+
+    def circumference(self):
+        """return circu,ference of a circle"""
+
+        return 2 * math.pi * self.radius
