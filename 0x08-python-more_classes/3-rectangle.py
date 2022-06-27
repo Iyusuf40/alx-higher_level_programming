f0dd204e9f1fc70465077213daadf3aaebd71f82
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-27 16:36:28 +0000

    def __str__

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""
+a rectangle class
+```example```
+>>> rec = Rectangle(2, 5)
+>>> rec.width
+2
+>>> rec.height
+5
+>>> rec.area()
+10
+>>> rec.perimeter()
+14
+>>> rec = Rectangle()
+>>> rec.width
+0
+>>> rec.height
+0
+>>> rec.area()
+0
+>>> rec.perimeter()
+0
+>>> print(rec)
+<BLANKLINE>
+>>> rec = Rectangle(2, 5)
+>>> print(rec)
+##
+##
+##
+##
+##
+
+"""
+
+
+class Rectangle:
+    '''an empty class'''
+
+    def __init__(self, width=0, height=0):
+        """initializes each instance"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """sets private attr __width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """width setter"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """sets private attr __height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """height setter"""
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        """area = width * height"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """perimeter = width + height"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width + self.height) * 2
+
+    def __str__(self):
+        """string rep"""
+        if self.width == 0 or self.height == 0:
+            return ""
+        cols = self.width * "#"
+        _str = ""
+        for _ in range(self.height):
+            _str += cols
+            if _ != self.height - 1:
+                _str += "\n"
+
+        return _str
