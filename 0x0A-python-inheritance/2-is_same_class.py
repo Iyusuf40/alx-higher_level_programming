5486e68fb0104e1def943d7e2aa32816c8aaeebe
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-07-05 02:52:35 +0000

    use type not isinstance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,4 +9,7 @@ is_same_class
 
 def is_same_class(obj, a_class):
     """returns True if same class else false"""
-    return isinstance(obj, a_class)
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
