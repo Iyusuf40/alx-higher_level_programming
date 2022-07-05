0791f2f8dbe000c402358ba7c1ed071ab602a33d
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-07-05 03:19:02 +0000

    add baseclass

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a base class of BaseGeometry"""
+
+
+class BaseGeometry:
+    """base class"""
+
+    def area(self):
+        """not yet implemented"""
+        raise TypeError("area() is not implemented")
