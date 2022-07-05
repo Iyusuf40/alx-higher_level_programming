e76b7d1bd14b12e1665998737194923f9d1d3048
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-07-05 02:46:23 +0000

    test

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,6 +4,9 @@ is_same_class
 """
 
 
+"""returns True if same class else false"""
+
+
 def is_same_class(obj, a_class):
     """returns True if same class else false"""
     return isinstance(obj, a_class)
