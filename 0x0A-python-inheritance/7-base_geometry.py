5f18421ef1f4be156a969ff74821e70336a8801a
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-07-05 03:59:11 +0000

    use string.format

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,8 +13,10 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates int"""
-        if not isinstance(value, int) and type(value) is not int:
+        """validates value
+        check if int type
+        """
+        if not isinstance(value, int) or type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
