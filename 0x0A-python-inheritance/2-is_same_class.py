6ddd4a1e11528d6fca5ed2bf5a9434ced69bb4ee
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-07-05 02:37:01 +0000

    add file2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+"""module has function
+is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if same class else false"""
+    return isinstance(obj, a_class)
