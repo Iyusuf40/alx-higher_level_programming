6b3a7ce287a0ba380da9f211b7bce17740649c9a
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-07-05 04:34:19 +0000

    add square class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""a base class of BaseGeometry"""
+
+
+class BaseGeometry:
+    """base class"""
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        """not yet implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value
+        check if int type
+        """
+        if not isinstance(value, int) or type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """rectangle class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """str rep"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area of the rectangle"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """str rep"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
