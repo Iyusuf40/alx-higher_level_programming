5aaf3aad6b07a35d158668ce6ba33235dc85d235
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-07-08 16:58:07 +0000

    add 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""module contains Studen class"""
+
+
+class Student:
+    """a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """mimics json.dumps"""
+        dct = {}
+
+        if attrs is None:
+            return self.__dict__
+
+        for item in attrs:
+            if item in self.__dict__:
+                dct[item] = self.__dict__[item]
+
+        return dct
+
+    def reload_from_json(self, json):
+        """deserializes json"""
+        self.__dict__ = json
