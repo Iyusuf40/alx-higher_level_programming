a1b3bc94995ae34b7f2b072fe05962fabfb75942
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-08-12 15:47:00 +0000

    add 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,12 +1,9 @@
 -- test
 -- query
 SELECT name FROM tv_genres
-INNER JOIN tv_show_genres AS tv_sg
-ON tv_genres.id IN (
-SELECT tv_show_genres.genre_id FROM tv_show_genres
-INNER JOIN tv_shows
-ON tv_show_genres.show_id = (
-SELECT tv_shows.id FROM tv_shows
-WHERE tv_shows.title = "Dexter"))
-GROUP BY name
-ORDER BY name; 
+LEFT JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter"
+ORDER BY name;
