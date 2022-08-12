a2432d1560321deae5cedb57079d7481d5bf304f
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-08-12 09:29:21 +0000

    add 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,6 @@
 SELECT tv_shows.title title, tv_show_genres.genre_id genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_show_genres.genre_id = NULL
-ORDER BY title, genre_id;
+ON tv_show_genres.genre_id = tv_shows.id
+WHERE genre_id IS NULL
+ORDER BY title;
