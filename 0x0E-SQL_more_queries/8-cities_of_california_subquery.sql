0eeb35c71f5c6b00861c382c3deea474e6fc6787
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-08-12 08:29:28 +0000

    fix query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- test
 -- query
 SELECT id, name FROM cities 
-WHERE cities.id = (SELECT id FROM states WHERE states.name = "California")
+WHERE cities.state_id = (SELECT id FROM states WHERE states.name = "California")
 ORDER BY id;
