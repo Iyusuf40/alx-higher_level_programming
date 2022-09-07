c0dc24ad83745e06030c1c1a015e11d6e96ac683
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-07 23:39:56 +0000

    fix bug

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,11 +8,16 @@ if (list.length < 4) {
   let big = ps(list[3]) >= ps(list[2]) ? ps(list[3]) : ps(list[2]);
   for (let index = 2; index < list.length; ++index) {
     if (ps(list[index]) > big) {
+      const save = big;
       big = ps(list[index]);
+      if (save > small) {
+        small = save;
+      }
     }
     if (ps(list[index]) > small && list[index] < big) {
       small = ps(list[index]);
     }
   }
   console.log(small);
+  console.log(big);
 }
