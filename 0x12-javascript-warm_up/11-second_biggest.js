6de98e838a1b9d66369717e6be1c206b1029640f
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-07 23:28:23 +0000

    add next big finder

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const list = process.argv;
+const ps = parseInt;
+if (list.length < 4) {
+  console.log(0);
+} else {
+  let small = ps(list[2]) <= ps(list[3]) ? ps(list[2]) : ps(list[3]);
+  let big = ps(list[3]) >= ps(list[2]) ? ps(list[3]) : ps(list[2]);
+  for (let index = 2; index < list.length; ++index) {
+    if (ps(list[index]) > big) {
+      big = ps(list[index]);
+    }
+    if (ps(list[index]) > small && list[index] < big) {
+      small = ps(list[index]);
+    }
+  }
+  console.log(small);
+}
