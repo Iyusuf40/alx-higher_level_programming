c72a2cde655b2007b51f8dd6d04b0849d4178348
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-12 17:21:26 +0000

    add 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" module contains sql query using dbapi"""
+
+
+import MySQLdb
+import sys
+
+
+def main():
+    """program entry"""
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+
+    conn = MySQLdb.connect(passwd=pwd, db=db, user=usr)
+    # conn = MySQLdb.connect(host="localhost", port=3306,
+    #                       user=usr, passwd=pwd,
+    #                       db=db, charset="utf8")
+    cur = conn.cursor()
+    query = "select * from cities order by cities.id"
+    cur.execute(query)
+    res = cur.fetchall()
+    for i in res:
+        print(i)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
