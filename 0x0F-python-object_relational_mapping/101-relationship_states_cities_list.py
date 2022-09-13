c32262373a95eb0f92f585372a263fe0622f4244
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-13 00:06:01 +0000

    add 101

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""practice module"""
+
+
+from relationship_state import State, Base
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def main():
+    """main function"""
+    ls = sys.argv[1:]
+    user = ls[0]
+    pwd = ls[1]
+    db = ls[2]
+    db_url = f"mysql+mysqldb://{user}:{pwd}@localhost/{db}"
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        city_list = state.cities
+        for city in city_list:
+            print(f"\t{city.id}: {city.name}")
+    session.commit()
+
+
+if __name__ == "__main__":
+    main()
