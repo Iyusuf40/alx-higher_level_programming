79772191dbb43e4642f80c7c4b9c4bccb7d9635c
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-22 18:31:46 +0000

    use silent option

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # sccript prints a sites body if status code is 200
-code=$(curl -Is "$1" | grep "HTTP*" | cut -d " " -f 2)
-if [ $code -eq 200 ]; then
-	curl -X GET "$1"
-fi
+curl -s $1
