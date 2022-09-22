21b7f61fcc3c8a37effaebfbdbdf0b18f269c0a5
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-22 23:09:37 +0000

    add 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""find_peak: finds a peak in an unsorted list"""
+
+
+def find_peak(lst):
+    """finds a peak in a list"""
+    if not lst:
+        return
+    state = []  # records trend and waits for change
+    current_peak = lst[0]
+    prev = current_peak
+    for item in lst:
+        if item > prev:
+            if item > current_peak:
+                current_peak = item
+            # state.append(1)
+            if len(state) > 2:
+                if prev < item:
+                    return current_peak
+            state.append(1)
+        else:
+            # state.append(0)
+            if len(state) > 2:
+                if prev > item:
+                    return current_peak
+            state.append(0)
+        prev = item
+    return current_peak
