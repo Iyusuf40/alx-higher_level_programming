2e774412342f2c71f3321a0ab76d5d7ddde19ebc
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-23 20:26:04 +0000

    add 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""http request with requests module"""
+
+
+import requests
+import sys
+
+
+def make_request():
+    """makes http request"""
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers["X-Request-Id"])
+
+
+if __name__ == "__main__":
+    make_request()
