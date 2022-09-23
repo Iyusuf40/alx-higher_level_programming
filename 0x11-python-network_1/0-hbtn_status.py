4a940b79c9c8c97083ece9fe656973a030506ffb
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-23 19:18:20 +0000

    add content line

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -14,8 +14,9 @@ def make_request():
         content = res.read()
         utf8 = str(content)
         print("Body response:")
-        print(f"\t - type: {type(content)}")
-        print(f"\t - utf8 content: {content.decode('utf-8')}")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
 
 
 if __name__ == "__main__":
