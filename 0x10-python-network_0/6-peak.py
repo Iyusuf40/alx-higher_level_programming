b7f687f2fd66b8200527dfd3caf317092f1ea7f7
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-23 00:10:45 +0000

    add 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,20 +9,16 @@ def find_peak(lst):
     idx = 0
     current_peak = lst[0]
     prev = current_peak
-    flag = 0
     len_ = len(lst)
     while idx < len_:
         item = lst[idx]
-        if item > prev:
-            if item > current_peak:
-                current_peak = item
+        if item > prev and item > current_peak:
+            current_peak = item
         if idx > 1:
             if prev > item and lst[idx - 2] < prev:
-                if flag == 1:
-                    return prev
-                return current_peak
+                return prev
             elif prev < item and lst[idx - 2] > prev:
-                flag = 1
+                return current_peak
         prev = item
         idx += 1
     return current_peak
