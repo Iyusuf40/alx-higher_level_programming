4555e34bc4b1f2bf863410f4ae24cb0605a529f8
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-23 20:32:19 +0000

    add 5

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""http request with requests module"""
+
+
+import requests
+import sys
+
+
+def make_request():
+    """makes http request"""
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {"email": email}
+    r = requests.get(url, data=data)
+    print(r.text)
+
+
+if __name__ == "__main__":
+    make_request()
