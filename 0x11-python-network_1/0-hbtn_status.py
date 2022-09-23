38512033eac6a924aa54152a17f3bd3ed0e287f7
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-09-23 19:15:16 +0000

    add readme

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module contains http request"""
+
+
+import urllib.request
+
+
+url = "https://alx-intranet.hbtn.io/status"
+
+
+def make_request():
+    """makes http request with usrllib"""
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        utf8 = str(content)
+        print("Body response:")
+        print(f"\t - type: {type(content)}")
+        print(f"\t - utf8 content: {content.decode('utf-8')}")
+
+
+if __name__ == "__main__":
+    make_request()
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+# Python networking continuation lesson
