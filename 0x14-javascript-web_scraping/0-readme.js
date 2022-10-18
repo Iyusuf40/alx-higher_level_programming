ece431f0bbda68b87c8e23d2ec76a9f49ab7b00b
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-18 15:31:52 +0000

    change node:fs to fs in import line

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* script reads a file and prints its content */
 
-const fs = require('node:fs');
+const fs = require('fs');
 const file = process.argv[2];
 fs.readFile(file, 'utf-8', (err, d) => {
   if (err) {
