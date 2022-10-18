a2ba1ba9797d29f0675e9edd2a66e42c7ff80bdb
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-18 17:54:45 +0000

    add filter by people id

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/* displays the status code of a request to url passed as arg */
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (!err) {
+    let count = 0;
+    const data = JSON.parse(body);
+    const list = data.results;
+    let index = 0;
+    for (index = 0; index < list.length; index++) {
+      if (checkFor18(list[index].characters).length === 1) {
+        count += 1;
+      }
+    }
+    console.log(count);
+  }
+});
+
+function checkFor18 (list) {
+  return (
+    list.filter((item) => {
+      return (item === 'https://swapi-api.hbtn.io/api/people/18/');
+    }
+    )
+  );
+}
